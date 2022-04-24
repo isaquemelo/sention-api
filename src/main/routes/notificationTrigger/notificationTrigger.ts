500323de9558f8e38ac7871262359da18112763d
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-04-24 13:37:47 -0300

    feat: Removed deviceId from notificationTriggers(POST, DELETE, PUT)

--- src/main/routes/notificationTrigger/notificationTrigger.ts
+++ src/main/routes/notificationTrigger/notificationTrigger.ts
@@ -9,15 +9,15 @@ export default class NotificationTriggerRoutes {
     public static buildRoutes(router: Router) {
         const notificationTriggerController: NotificationTriggerController = makeNotificationTriggerController()
 
-        router.post('/user/devices/:deviceId/sensors/:sensorId/notificationTrigger', AuthenticationdMiddleware, (req, res) => {
+        router.post('/user/devices/sensors/:sensorId/notificationTrigger', AuthenticationdMiddleware, (req, res) => {
             return notificationTriggerController.saveNotificationTrigger(req, res)
         })
 
-        router.delete('/user/devices/:deviceId/sensors/:sensorId/notificationTrigger/:notificationTriggerId', AuthenticationdMiddleware, (req, res) => {
+        router.delete('/user/devices/sensors/:sensorId/notificationTrigger/:notificationTriggerId', AuthenticationdMiddleware, (req, res) => {
             return notificationTriggerController.deleteNotificationTrigger(req, res)
         })
 
-        router.put('/user/devices/:deviceId/sensors/:sensorId/notificationTrigger/:notificationTriggerId', AuthenticationdMiddleware, (req, res) => {
+        router.put('/user/devices/sensors/:sensorId/notificationTrigger/:notificationTriggerId', AuthenticationdMiddleware, (req, res) => {
             return notificationTriggerController.updateNotificationTrigger(req, res)
         })
     }
