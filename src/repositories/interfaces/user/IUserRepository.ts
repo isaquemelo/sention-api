f61d0c400fd528b2d1630b54e61843feb43b7b1c
Isaque Melo <isaquegbmelo2@gmail.com>
2022-06-13 20:31:49 -0300

    fix: Resolve ts error

--- src/repositories/interfaces/user/IUserRepository.ts
+++ src/repositories/interfaces/user/IUserRepository.ts
@@ -1,11 +1,11 @@
-import Device from "../../entities/Device";
-import User from "../../entities/User";
+import Device from "../../../entities/Device";
+import User from "../../../entities/User";
 import { IUserFindingCriterias } from "./IUserFindingCriterias";
 
 
 export interface IUserRepository {
     findOne(params: IUserFindingCriterias): Promise<User | false>;
     save(user: User): Promise<User | false>;
-    associateDeviceToUser(accessCode: string, userId: string): Promise<Device | false>;
+    associateDeviceToUser(accessCode: string, userId: string): Promise<boolean>;
     dissociateDevice(deviceId: string, userId: string): Promise<boolean>;
 }
